87a55ebf122412b13a65e37e4559af7fcad56a53
johnmiicheal <michealelijah301@gmail.com>
2024-07-25 13:12:26 +0100

    setState --refresh

--- src/components/Index/Projects.tsx
+++ src/components/Index/Projects.tsx
@@ -1,4 +1,15 @@
-import { Flex, Icon, Badge, Text, Image, Button, IconButton, Box, SimpleGrid, useMediaQuery } from "@chakra-ui/react";
+import {
+  Flex,
+  Icon,
+  Badge,
+  Text,
+  Image,
+  Button,
+  IconButton,
+  Box,
+  SimpleGrid,
+  useMediaQuery,
+} from "@chakra-ui/react";
 import { IoLinkOutline, IoLogoGithub } from "react-icons/io5";
 import { PiCircuitryLight } from "react-icons/pi";
 import client from "@/utils/sanity";
@@ -8,14 +19,10 @@ import { useEffect, useState } from "react";
 
 export const Projects = () => {
   const [projects, setProjects] = useState<any[]>([]);
-  const [mobile] = useMediaQuery("(max-width: 500px)");
-  const [items, setItems] = useState(2)
-
+  const [mobile] = useMediaQuery("(max-width: 1024px)");
+  const [items, setItems] = useState(2);
 
   useEffect(() => {
-    if(mobile){
-      setItems(3)
-    }
     const fetchProjects = async () => {
       const project = await client.fetch(`
         *[_type == "project"]{_key,title,publishedAt,slug,summary,authorAffiliation, "mainImage" : mainImage{alt,caption, asset{_ref}->{url,"blurHash":metadata.blurHash}}} | order(publishedAt desc)[0..${items}]
@@ -23,13 +30,17 @@ export const Projects = () => {
       setProjects(project);
     };
     fetchProjects();
+    if (mobile) {
+      setItems(3);
+      fetchProjects();
+    }
   }, []);
   return (
     <Flex
       direction="column"
       w="full"
       h="full"
-      align={{ base: "center", lg: "start"}}
+      align={{ base: "center", lg: "start" }}
       mt={10}
       pb={20}
     >
@@ -37,33 +48,40 @@ export const Projects = () => {
         <Icon as={PiCircuitryLight} color="#479AFB" />
         <Text>Projects</Text>
       </Flex>
-      <SimpleGrid columns={{ base: 1, md: 2, '2xl': 3 }} spacing="10">
+      <SimpleGrid columns={{ base: 1, md: 2, "2xl": 3 }} spacing="10">
         {projects?.map(
-                  ({
-                    title,
-                    authorAffiliation,
-                    summary,
-                    _key,
-                    publishedAt,
-                    slug,
-                    mainImage,
-                  }: any) => {
-                    return (
-                        <ProjectListItem
-                          key={_key}
-                          title={title}
-                          authors={"Nelson"}
-                          summary={summary}
-                          publishedAt={getDateString(publishedAt)}
-                          slug={slug}
-                          mainImage={mainImage}
-                        />
-                    );
-                  }
-                )}
+          ({
+            title,
+            authorAffiliation,
+            summary,
+            _key,
+            publishedAt,
+            slug,
+            mainImage,
+          }: any) => {
+            return (
+              <ProjectListItem
+                key={_key}
+                title={title}
+                authors={"Nelson"}
+                summary={summary}
+                publishedAt={getDateString(publishedAt)}
+                slug={slug}
+                mainImage={mainImage}
+              />
+            );
+          }
+        )}
       </SimpleGrid>
-       
-      <Button mt={3} variant="link" colorScheme="blue" onClick={() => window.location.assign('/projects')}>See more projects</Button>
+
+      <Button
+        mt={3}
+        variant="link"
+        colorScheme="blue"
+        onClick={() => window.location.assign("/projects")}
+      >
+        See more projects
+      </Button>
     </Flex>
   );
 };
