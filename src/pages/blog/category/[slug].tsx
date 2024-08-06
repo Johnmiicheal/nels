627ce6e8cf0521224e5f8c4411765f1b09339836
Elijah Johnmicheal <michealelijah301@gmail.com>
2024-08-06 13:11:04 +0100

    pending order

--- src/pages/blog/category/[slug].tsx
+++ src/pages/blog/category/[slug].tsx
@@ -12,35 +12,35 @@ const Category = () => {
     const [slug, setSlug] = useState<string>(asPath.split('/').filter(Boolean).pop()!)
     const [content, setContent] = useState<any[]>([]);
 
-    useEffect(() => {
-        setSlug(asPath.split('/').filter(Boolean).pop()!)
-        const fetchContent = async () => {
-            const articles = await client.fetch(
-                `*[count((categories[]->slug.current)[@ in ["${slug}"]]) > 0 && hidden != true]{_id,publishedAt,title,slug, body[]{
-                    ...,
-                    _type == 'block' => {
-                    ...,
-                    markDefs[]{
-                      ...,
-                      _type == "internalLink" => {
-                        "info": @.reference->{_type,slug}
-                      }
-                    }
-                  },
-                 _type == 'image' => {
-                    type,alt,_key,_type, asset{_ref}-> {url,"blurHash":metadata.blurHash, "dimensions":metadata.dimensions{width,height}}
-                  },} } | order(publishedAt desc)[0...5]`
-                  );
-              setContent(articles)
-        }
-        if (content.length < 1) {
-           fetchContent()
-          }
-    }, [content, slug])
+    // useEffect(() => {
+    //     setSlug(asPath.split('/').filter(Boolean).pop()!)
+    //     const fetchContent = async () => {
+    //         const articles = await client.fetch(
+    //             `*[count((categories[]->slug.current)[@ in ["${slug}"]]) > 0 && hidden != true]{_id,publishedAt,title,slug, body[]{
+    //                 ...,
+    //                 _type == 'block' => {
+    //                 ...,
+    //                 markDefs[]{
+    //                   ...,
+    //                   _type == "internalLink" => {
+    //                     "info": @.reference->{_type,slug}
+    //                   }
+    //                 }
+    //               },
+    //              _type == 'image' => {
+    //                 type,alt,_key,_type, asset{_ref}-> {url,"blurHash":metadata.blurHash, "dimensions":metadata.dimensions{width,height}}
+    //               },} } | order(publishedAt desc)[0...5]`
+    //               );
+    //           setContent(articles)
+    //     }
+    //     if (content.length < 1) {
+    //        fetchContent()
+    //       }
+    // }, [content, slug])
 
     return(
         <PageProvider title={slug}>
-            <Stack>
+{/*             <Stack>
                 {content.map((item, index) => (
                     <Stack key={index} gap={5}>
                     <SinglePost title={item?.title!}
@@ -50,7 +50,12 @@ const Category = () => {
                     <Divider my={4} />
                     </Stack>
                 ))}
-            </Stack>
+            </Stack> */}
+            <Flex direction="column" align="center" mt={20}>
+                <Text color="red" fontSize="32" _hover={{ bg: "yellow" }}>Nelson is owing me money!!!</Text>
+                <Image src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbXE2b2xsdjc1b2d0eGl4MHJvcndlOHM4aXh0YjliZG00bzgwNXUyOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/uyWTOgNGGWfks/giphy.webp" alt="money money money" />
+                <Button bg="blue" color="white" _hover={{bg: "green"}} mt={10} onClick={() => window.location.assign('/')}>Go Back to Home Page</Button>
+            </Flex>
         </PageProvider>
     )
 }
